089d78ed542777df83685d6f2a72e513fa71ce58
Dellayunia <dellayunia1414@gmail.com>
2025-01-01 14:24:50 +0700

    menambahkan perubahan pada file routing dan summary page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,8 +38,8 @@ const routes: Routes = [
   {
     path: 'mapsbaterai',
     loadChildren: () => import('./mapsbaterai/mapsbaterai.module').then( m => m.MapsbateraiPageModule)
-  },  {
-<<<<<<< HEAD
+  },
+  {
     path: 'reviewsummary',
     loadChildren: () => import('./reviewsummary/reviewsummary.module').then( m => m.ReviewsummaryPageModule)
   },
@@ -47,8 +47,7 @@ const routes: Routes = [
     path: 'summary',
     loadChildren: () => import('./summary/summary.module').then( m => m.SummaryPageModule)
   },
-
-=======
+  {
     path: 'afterpayment',
     loadChildren: () => import('./afterpayment/afterpayment.module').then( m => m.AfterpaymentPageModule)
   },
@@ -56,56 +55,7 @@ const routes: Routes = [
     path: 'historytransaksi',
     loadChildren: () => import('./historytransaksi/historytransaksi.module').then( m => m.HistorytransaksiPageModule)
   },
-
-<<<<<<< HEAD
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'landing',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'landing',
-    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'set-up-profile',
-    loadChildren: () => import('./set-up-profile/set-up-profile.module').then( m => m.SetUpProfilePageModule)
-  },
-  {
-    path: 'rental',
-    loadChildren: () => import('./rental/rental.module').then( m => m.RentalPageModule)
-  },
-  {
-    path: 'activities',
-    loadChildren: () => import('./activities/activities.module').then( m => m.ActivitiesPageModule)
-  },
-  {
-    path: 'mapsbaterai',
-    loadChildren: () => import('./mapsbaterai/mapsbaterai.module').then( m => m.MapsbateraiPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
-    path: 'editprofile',
-    loadChildren: () => import('./editprofile/editprofile.module').then( m => m.EditprofilePageModule)
-  },
-];
-
+]
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
@@ -113,18 +63,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
->>>>>>> 19db8cf6d843f5092d6583f502d84a28aa668561
-];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-<<<<<<< HEAD
-=======
->>>>>>> a61b581e37762846cbd8a6dd77ee979d0d632fde
->>>>>>> 19db8cf6d843f5092d6583f502d84a28aa668561
