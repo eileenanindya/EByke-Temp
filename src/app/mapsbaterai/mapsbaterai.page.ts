68df58c83639a3a6fc311a057918c0c0987cc8b6
Dellayunia <dellayunia1414@gmail.com>
2025-01-10 11:07:00 +0700

    tracking mapsbaterai

--- src/app/mapsbaterai/mapsbaterai.page.ts
+++ src/app/mapsbaterai/mapsbaterai.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet'
+import * as L from 'leaflet';
+import 'leaflet-routing-machine'; 
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 
 @Component({
@@ -10,107 +11,127 @@ import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 export class MapsbateraiPage implements OnInit {
   map!: L.Map;
   userMarker!: L.Marker;
-  constructor(private geolocation: Geolocation) { }
+  userLocation: L.LatLng | null = null; 
+  routeControl!: L.Routing.Control; 
+  destinationMarker: L.Marker | null = null; 
+  distance: string = '0.00 km';
+  userLocationText: string = '';
+  userLocationDistance: string = ''; 
+  destinationText: string = 'Select Destination';
 
-  ngOnInit() {
-    
-  }
+  constructor(private geolocation: Geolocation) {}
 
-  ionViewDidEnter(){
+  ngOnInit() {}
+
+  ionViewDidEnter() {
     this.map = L.map('mapId').setView([-7.9806, 112.6323], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
 
-  //   var myIcon = L.icon({
-  //     iconUrl: 'assets/marker-icon.png',
-  //     iconSize: [38, 55],
-  //     iconAnchor: [22, 94],
-  //     popupAnchor: [-3, -76],
-  //     shadowUrl: 'assets/marker-shadow.png',
-  //     shadowSize: [68, 95],
-  //     shadowAnchor: [22, 94]
-  // });
-  // Ikon marker khusus
-  const myIcon = L.icon({
-    iconUrl: 'assets/marker-icon.png',
-    iconSize: [38, 55],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: 'assets/marker-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94],
-  });
+    const myIcon = L.icon({
+      iconUrl: 'assets/marker-icon.png',
+      iconSize: [38, 55],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowUrl: 'assets/marker-shadow.png',
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94],
+    });
 
-  const myIcon2 = L.icon({
-    iconUrl: 'assets/icon/yourlocation.png', // Ikon your location
-    iconSize: [38, 55],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: 'assets/marker-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-  });
-  
+    const myIcon2 = L.icon({
+      iconUrl: 'assets/icon/yourlocation.png',
+      iconSize: [38, 55],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowUrl: 'assets/marker-shadow.png',
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94],
+    });
 
-  // L.marker([50.505, 30.57], {icon: myIcon  }).addTo(map);
+    // Marker titik tujuan
+    const markPoint1 = L.marker([-7.9900, 112.6400], { icon: myIcon });
+    markPoint1.bindPopup('Pos 1: Jl. Mayjend Panjaitan No.75, Penanggungan, Kec. Klojen, Kota Malang').addTo(this.map);
+    markPoint1.on('click', () => this.calculateRoute(markPoint1.getLatLng(), 'Marker 1', 'Jl. Mayjend Panjaitan No.75, Penanggungan, Kec. Klojen, Kota Malang'));
 
-  const markPoint1 = L.marker([-7.9900, 112.6400], { icon: myIcon }); // Lokasi baru
-  markPoint1.bindPopup('Marker 1: New location!').addTo(this.map);
+    const markPoint2 = L.marker([-7.9856, 112.6423], { icon: myIcon });
+    markPoint2.bindPopup('Pos 2: Jl. Raya Tlogomas, Malang, Jawa Timur').addTo(this.map);
+    markPoint2.on('click', () => this.calculateRoute(markPoint2.getLatLng(), 'Marker 2', 'Jl. Raya Tlogomas, Malang, Jawa Timur'));
 
-  // Menambahkan marker kedua
-  const markPoint2 = L.marker([-7.9856, 112.6423], { icon: myIcon });
-  markPoint2.bindPopup('Marker 2: A pretty CSS popup.<br> Easily customizable.').addTo(this.map);
+    const markPoint3 = L.marker([-7.9906, 112.6523], { icon: myIcon });
+    markPoint3.bindPopup('Pos 3: Jl. Soekarno Hatta, Malang, Jawa Timur').addTo(this.map);
+    markPoint3.on('click', () => this.calculateRoute(markPoint3.getLatLng(), 'Marker 3', 'Jl. Soekarno Hatta, Malang, Jawa Timur'));
 
-  // Menambahkan marker ketiga
-  const markPoint3 = L.marker([-7.9906, 112.6523], { icon: myIcon });
-  markPoint3.bindPopup('Marker 3: A pretty CSS popup.<br> Easily customizable.').addTo(this.map);
-
-  const markPointBlimbing = L.marker([-7.9910, 112.6280], { icon: myIcon });
-  markPointBlimbing.bindPopup('Kecamatan Blimbing').addTo(this.map);
-
-  const markPointKlojen = L.marker([-8.0166, 112.6320], { icon: myIcon });
-  markPointKlojen.bindPopup('Kecamatan Klojen').addTo(this.map);
-
-  const markPointLowokwaru = L.marker([-7.9867, 112.6285], { icon: myIcon });
-  markPointLowokwaru.bindPopup('Kecamatan Lowokwaru').addTo(this.map);
-
-  const markPointSukun = L.marker([-7.9862, 112.6200], { icon: myIcon });
-  markPointSukun.bindPopup('Kecamatan Sukun').addTo(this.map);
-
-  const markPointDau = L.marker([-8.0380, 112.6000], { icon: myIcon });
-  markPointDau.bindPopup('Kecamatan Dau').addTo(this.map);
-
-  
-//     const markPoint = L.marker([-7.9806, 112.6323], {icon: myIcon});
-//        markPoint .bindPopup('A pretty CSS popup.<br> Easily customizable.').addTo(this.map)
-
-//        const markPoint2 = L.marker([-7.9366016859985695, 112.62538238591131], {icon: myIcon});
-//        markPoint .bindPopup('A pretty CSS popup.<br> Easily customizable.').addTo(this.map)
-// this.map.addLayer(markPoint) 
-
-    // Tambahkan lokasi real-time
+    // Lokasi real-time pengguna
     this.geolocation.watchPosition().subscribe((position) => {
-      if (position && 'coords' in position) { // Memeriksa apakah position memiliki properti coords
+      if (position && 'coords' in position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-    
-        const realtimeMarker = L.marker([latitude, longitude], { icon: myIcon2 });
-        realtimeMarker
-          .bindPopup(`You are here: ${latitude.toFixed(5)}, ${longitude.toFixed(5)}`)
-          .addTo(this.map);
-    
+
+        this.userLocation = L.latLng(latitude, longitude);
+        this.userLocationText = `You are here: ${latitude.toFixed(5)}, ${longitude.toFixed(5)}`;
+
+        if (this.userMarker) {
+          this.map.removeLayer(this.userMarker); // Hapus marker sebelumnya
+        }
+
+        this.userMarker = L.marker([latitude, longitude], { icon: myIcon2 });
+        this.userMarker.bindPopup(this.userLocationText).addTo(this.map);
+
         this.map.setView([latitude, longitude], 15);
       } else {
         console.error('Geolocation error:', position);
       }
-    });   
+    });
+  }
 
-   }
+  // Fungsi untuk menghitung rute
+  calculateRoute(destination: L.LatLng, markerName: string, markerAddress: string) {
+    if (!this.userLocation) {
+      alert('User location not available yet.');
+      return;
+    }
 
+    // Hapus rute sebelumnya
+    if (this.routeControl) {
+      this.map.removeControl(this.routeControl);
+    }
+
+    // Buat rute dari lokasi pengguna ke titik tujuan
+    this.routeControl = L.Routing.control({
+      waypoints: [this.userLocation, destination],
+      routeWhileDragging: true,
+      showAlternatives: false,
+      addWaypoints: false,
+      lineOptions: {
+        styles: [{ color: '#0074D9', weight: 5 }],
+        extendToWaypoints: true, // Menambahkan properti extendToWaypoints
+        missingRouteTolerance: 10, // Menambahkan properti missingRouteTolerance
+      },
+    }).addTo(this.map);
+    
+
+    // Sembunyikan instruksi navigasi
+    const routingContainer = document.querySelector('.leaflet-routing-container');
+    if (routingContainer) {
+      routingContainer.setAttribute('style', 'display: none;');
+    }
+
+    // Hitung jarak rute
+    this.routeControl.on('routesfound', (event: any) => {
+      const distanceInKm = event.routes[0].summary.totalDistance / 1000; // Dalam kilometer
+      this.distance = `${distanceInKm.toFixed(2)} km`;
+
+      // Update tampilan informasi jarak
+      this.destinationText = markerAddress;
+      this.userLocationDistance = ''; // Set menjadi string kosong
+
+      // Update tampilan lokasi dan jarak
+      const distanceElement = document.getElementById('distance');
+      if (distanceElement) {
+        distanceElement.textContent = this.distance;
+      }
+    });
+  }
 }
-
-
-
-
